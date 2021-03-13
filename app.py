1e2815a0442105764daf5747b55629082752ec1a
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-13 23:54:13 +0330

    set SQLALCHEMY DATABASE URI

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .info import username, password
 from users.routes import users
 
 
 app = Flask(__name__)
 app.register_blueprint(users)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{username}:{password}@localhost/db'
 
 
 @app.route('/')
