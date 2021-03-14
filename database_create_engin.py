bb4b157258a9cb21f5426151baa5061f8803aab5
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-14 16:49:20 +0330

    create database engin

---
+++ database_create_engin.py
@@ -0,0 +1,17 @@
+from info import username, password, host
+import mysql.connector
+
+
+db_connector = mysql.connector.connect(
+    host=host, user=username, passwd=password,)
+
+my_cursor = db_connector.cursor()
+
+my_cursor.execute("CREATE DATABASE mysqldb")
+
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
+
+my_cursor.close()
+print('All Done!')
