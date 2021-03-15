2f6f8152df90883387c75778dbc278d69930c7fd
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-15 18:04:34 +0330

    import SecretKey

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from info import username, password, host, database
 from users.routes import users
+from utility import SecretKey
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SecretKey
 app.register_blueprint(users)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/{database}'
 db = SQLAlchemy(app)
