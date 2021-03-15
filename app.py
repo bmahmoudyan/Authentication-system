ec2d41fee38ad02a982eac643331129327839e5a
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-15 18:03:01 +0330

    delete start  function

--- app.py
+++ app.py
@@ -3,16 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from info import username, password, host, database
 from users.routes import users
 
-
 app = Flask(__name__)
 app.register_blueprint(users)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/{database}'
 db = SQLAlchemy(app)
-
-
-@app.route('/')
-def start():
-    return 'Hello world!'
 
 
 if __name__ == '__main__':
