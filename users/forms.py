8752cdccfa5e16681eccc048d157f3150b5cbada
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-15 23:27:12 +0330

    upgrade forms.py

--- users/forms.py
+++ users/forms.py
@@ -4,9 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()],  render_kw={
-                           "placeholder": "email username phone number"})
+    login_method = StringField('username', validators=[DataRequired()],  render_kw={
+                               "placeholder": "email username phone number", 'id': 'inputEmail4', "class": 'form-control'})
     password = PasswordField(' password', validators=[DataRequired()],  render_kw={
-                             "placeholder": "password"})
-
-    submit = SubmitField(render_kw={"value": "GO"})
+                             "placeholder": "password", 'class': "form-control", 'id': 'inputPassword4'})
+    submit = SubmitField(render_kw={"value": "GO", 'class': 'btn btn-primary'})
