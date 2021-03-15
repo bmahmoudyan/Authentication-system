b20bd9bb91154efbab7a98ab79c849f515f8863e
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-15 14:59:22 +0330

    set database name in info.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from info import username, password, host
+from info import username, password, host, database
 from users.routes import users
 
 
 app = Flask(__name__)
 app.register_blueprint(users)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/mysqldb'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/{database}'
 db = SQLAlchemy(app)
 
 
