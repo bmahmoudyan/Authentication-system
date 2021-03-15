083c5a5893f367b7a728b28d71fad9d5660f87df
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-15 15:04:23 +0330

    create register view in users.routes

--- users/routes.py
+++ users/routes.py
@@ -7,3 +7,8 @@ users = Blueprint('users', __name__)
 @users.route('/login')
 def login_view():
     return 'login page'
+
+
+@users.route('/register')
+def register_view():
+    return 'register page'
