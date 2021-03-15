6770debeb7e40694a50fa4247825ae041d98ef35
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-15 16:38:19 +0330

    change view names & import render_template

--- users/routes.py
+++ users/routes.py
@@ -1,14 +1,14 @@
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 users = Blueprint('users', __name__)
 
 
 @users.route('/login')
-def login_view():
-    return 'login page'
+def login():
+    return render_template('users/login.html')
 
 
 @users.route('/register')
-def register_view():
+def register():
     return 'register page'
