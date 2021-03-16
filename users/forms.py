447b9de499125c3996268cc22a7372bd748b72a5
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-16 11:53:56 +0330

    update place holders

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     login_method = StringField('username', validators=[DataRequired()],  render_kw={
-                               "placeholder": "email username phone number", 'id': 'inputEmail4', "class": 'form-control'})
+                               "placeholder": "email username or phone number", 'id': 'inputEmail4', "class": 'form-control'})
     password = PasswordField(' password', validators=[DataRequired()],  render_kw={
                              "placeholder": "password", 'class': "form-control", 'id': 'inputPassword4'})
-    submit = SubmitField(render_kw={"value": "GO", 'class': 'btn btn-primary'})
+    submit = SubmitField(render_kw={"value": "LOGIN", 'class': 'btn btn-primary'})
