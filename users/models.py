3856cf12aa7e0501fa3ab8870e7a0c6c7cda839d
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-16 14:02:06 +0330

    update user model

--- users/models.py
+++ users/models.py
@@ -7,11 +7,11 @@ class User(db.Model):
     name = db.Column(db.String(80), nullable=True)
     last_name = db.Column(db.String(80), nullable=True)
     username = db.Column(db.String(120), nullable=False, unique=True)
-    email_addr = db.Column(db.String(80), nullable=False, unique=True)
-    email_addr_is_Confirm = db.Column(db.Boolean)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    email_is_Confirm = db.Column(db.Boolean)
     phone_number = db.Column(db.String(80), nullable=False, unique=True)
     phone_number_is_Confirm = db.Column(db.Boolean)
-    password = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return f'{id}, {self.name}, {self.username}, {self.email_addr}:{self.email_addr_is_Confirm}, {self.phone_number}:{self.phone_number_is_Confirm}'
