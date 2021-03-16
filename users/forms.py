cb17263504a159e46913ecf5dce8fa15665cd52c
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-16 14:01:52 +0330

    RegisterForm

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +10,13 @@ class LoginForm(FlaskForm):
     password = PasswordField(' password', validators=[DataRequired()],  render_kw={
                              "placeholder": "password", 'class': "form-control", 'id': 'inputPassword4'})
     submit = SubmitField(render_kw={"value": "LOGIN", 'class': 'btn btn-primary'})
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=3, max=25)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=3, max=25)])
+    username = StringField('Email Address')
+    email = EmailField('Email Addres', validators=[DataRequired()])
+    phone_number = StringField('Number')
+    password = PasswordField(' password', validators=[DataRequired()],  render_kw={"placeholder": "password", 'class': "form-control", 'id': 'inputPassword4'})
+    confirm_password = PasswordField(' password', validators=[DataRequired()],  render_kw={"placeholder": "password", 'class': "form-control", 'id': 'inputPassword4'})
