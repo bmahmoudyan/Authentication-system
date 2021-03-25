736c738b091571cccd4c97e529ae17e658959409
bny1485 <benyaminmahmoudyan@gmail.com>
2021-03-25 18:59:22 +0430

    import main app

--- app.py
+++ app.py
@@ -12,5 +12,8 @@ db = SQLAlchemy(app)
 from users.routes import users
 app.register_blueprint(users)
 
+from main.routes import main
+app.register_blueprint(main)
+
 if __name__ == '__main__':
     app.run()
