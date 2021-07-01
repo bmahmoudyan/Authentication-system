2808d3ce4dd66e4e45a1a346e19ca00364fac952
Benyamin <benyaminmahmoudyan@gmail.com>
2021-07-02 00:32:21 +0430

    user commit to data base

--- users/routes.py
+++ users/routes.py
@@ -1,6 +1,8 @@
-
-from flask import Blueprint, render_template
+from app import db
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
 from users.forms import LoginForm, RegisterForm
+from users.models import User
 
 users = Blueprint('users', __name__)
 
@@ -23,4 +25,9 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         print('register is validate_on_submit')
+        new_user = User(name=form.name.data, last_name=form.last_name.data, username=form.username.data,
+                        email=form.email.data, phone_number=form.phone_number.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('users.login'))
     return render_template('users/register.html', form=form)
