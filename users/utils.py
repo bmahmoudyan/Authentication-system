b4d483ba9727e743d83fc666918a2c508c7c7566
Benyamin <benyaminmahmoudyan@gmail.com>
2021-07-16 20:20:46 +0430

    write confirm generator function

---
+++ users/utils.py
@@ -0,0 +1,11 @@
+
+from info import kavenegarApi
+from kavenegar import *
+from random import randint
+
+
+def confirm_code_generator():
+    confirmCode = randint(000000, 999999)
+    return int(confirmCode)
+
+
