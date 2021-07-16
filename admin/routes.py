55df37aaeef77c4a92374927e991572f1e5ff950
Benyamin <benyaminmahmoudyan@gmail.com>
2021-07-16 12:41:02 +0430

    add TODO

--- admin/routes.py
+++ admin/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 
 admin = Blueprint('admin', __name__)
 
-
+# TODO jast admin can open this page
 @admin.route('/administrator')
 def administrator():
     context = {
