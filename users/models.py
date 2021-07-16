4db546be63d22b4c97ce9bc19c22c5837d464e81
Benyamin <benyaminmahmoudyan@gmail.com>
2021-07-16 20:19:13 +0430

    add UserConfirmCode model

--- users/models.py
+++ users/models.py
@@ -21,3 +21,9 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f'{id}, {self.username}, {self.email}'
+
+
+class UserConfirmCode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    code = db.Column(db.String(6))
