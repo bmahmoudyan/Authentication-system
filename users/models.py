ca093ea6cc223c710fdbc09f91869133fc2a1df0
Benyamin <benyaminmahmoudyan@gmail.com>
2021-07-17 00:49:54 +0430

    add defulte false to user model confirm columns

--- users/models.py
+++ users/models.py
@@ -14,9 +14,9 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(80), nullable=True)
     username = db.Column(db.String(120), nullable=False, unique=True)
     email = db.Column(db.String(180), nullable=False, unique=True)
-    email_is_Confirm = db.Column(db.Boolean, nullable=True)
+    email_is_Confirm = db.Column(db.Boolean, nullable=False, default=False)
     phone_number = db.Column(db.String(80), nullable=False, unique=True)
-    phone_number_is_Confirm = db.Column(db.Boolean, nullable=True)
+    phone_number_is_Confirm = db.Column(db.Boolean, nullable=False, default=False)
     password = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
