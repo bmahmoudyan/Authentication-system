77ac7e82dd38833180b8058278483694f889a52a
Benyamin <benyaminmahmoudyan@gmail.com>
2021-07-16 20:32:22 +0430

    send confirm code to user with sms

--- users/routes.py
+++ users/routes.py
@@ -1,8 +1,9 @@
 from app import db
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from users.forms import LoginForm, RegisterForm
-from users.models import User
+from users.models import User, UserConfirmCode
+from users.utils import confirm_code_generator, sms_sender
 
 users = Blueprint('users', __name__)
 
@@ -45,9 +46,19 @@ def profile(username):
 
 
 # TODO confirm user phone number
-@users.route('/confirmPhoneNumber')
-def confirm_phone_number():
-    return render_template('users/confirmPhoneNumber.html')
+@users.route('/user/<string:username>/confirmPhoneNumber', methods=['POST', 'GET'])
+def confirm_phone_number(username):
+    confirm_code = confirm_code_generator()
+
+    if request.method == 'GET':
+        user = User.query.filter_by(username=username).first()
+        name, last_name, phone_number = user.name, user.last_name, user.phone_number
+        msg = f"سلام {name} {last_name} به سایت ما خوش آمدید کد تایید شما : {confirm_code}‌"
+        send = sms_sender(phone_number, msg)
+        new_code = UserConfirmCode(username=username, code=confirm_code)
+        db.session.add(new_code)
+        db.session.commit()
+        return render_template('users/confirmPhoneNumber.html')
 
 
 # TODO confirm user email address
